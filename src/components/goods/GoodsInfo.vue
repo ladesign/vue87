7d90cd57e6946a8df1756c124483f1b1d8a251b7
ladesign <jeywang@gmail.com>
2019-08-28 17:28:25 +0800

    day10 更新

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -12,7 +12,8 @@
       @before-enter="beforeEnter"
       @enter="enter"
       @after-enter="afterEnter">
-      <div class="ball" v-show="ballFlag" ref="ball"></div>
+      <div class="ball" v-show="ballFlag" ref="ball"></div>  
+      <!-- ball是屬於當前元件，可以用 ref屬性 來讓js可以指到此dom -->
     </transition>
 
     <!-- 
@@ -39,12 +40,17 @@
           </p>
           <!-- 3、數字選擇框元件_使用 -->
           <p>購買數量：     
-            <numbox></numbox> 
-            <!-- <numbox @getcount="getSelectedCount" :max="goodsinfo.stock_quantity"></numbox> -->
+            <!-- <numbox></numbox>  -->
+            <numbox @getcount="getSelectedCount" :max="goodsinfo.stock_quantity"></numbox>
           </p>
           <p>
             <mt-button type="primary" size="small">立即購買</mt-button>
             <mt-button type="danger" size="small" @click="addToShopCar">加入購物車</mt-button>
+            <!-- 分析： 如何實現 加入購物車 時候，拿到 選擇的數量 -->
+            <!-- 1. 經過分析發現： 按鈕屬於 goodsinfo 頁面(父)， 數字 屬於 numberbox 元件(子) -->
+            <!-- 2. 由於涉及到了 父、子元件的嵌套 了，所以，無法直接在 goodsinfo 頁面中獲取到 選中的商品數量值-->
+            <!-- 3. 怎麼解決這個問題：涉及到了 子元件 向 父元件 傳值了（事件調用機制） -->
+            <!-- 4. 事件調用的本質： 父向子傳遞方法，子調用這個方法， 同時把 數據當作參數 傳遞給這個方法。(參考 day4/筆記/父元件向子元件傳遞方法來獲取子元件的值(子元件向父元件傳值) -->
           </p>  
 
         </div>
@@ -84,9 +90,12 @@ export default {
       id: this.$route.params.id, // 將路由參數對像中的 id 掛載到 data , 方便後期呼叫
       lunbotu: [], // 輪播圖的數據
       goodsinfo: {}, // 獲取到的商品的資訊
+      
       // 控制小球的隱藏和顯示的識別符號
       // ballFlag: true, // 顯示小球，調整小球位置用
-      ballFlag: false  // 未點擊加到購物車按鈕前先隱藏小球
+      ballFlag: false,  // 未點擊加到購物車按鈕前先隱藏小球
+
+      selectedCount: 1 // 儲存使用者選中的商品數量， 預設，認為使用者買1個
 
     };
   },
@@ -134,14 +143,47 @@ export default {
     },
     enter(el, done) {// 動畫鉤子函數，動畫入場
       el.offsetWidth; // 這句話，沒有實際的作用，但是，如果不寫，出不來動畫效果，把它視為強制動畫重新整理
-      el.style.transform = "translate(56px, 430px)"; //移到終點，這邊終點位置是手動調整出來固定的，在不同尺寸螢幕下不準確
+
+      // 小球動畫基本：
+      // el.style.transform = "translate(56px, 430px)"; //移到終點，這邊終點位置是手動調整出來固定的，在不同尺寸螢幕下不準確
+
+      // 小球動畫優化思路：
+      // 1. 先分析導致 動畫 不準確的 本質原因： 我們把 小球 最終 位移到的 位置，已經侷限在了某一解析度下的 滾動條未滾動的情況下；
+      // 2. 只要解析度和 測試的時候不一樣，或者 滾動條有一定的滾動距離之後， 問題就出現了；
+      // 3. 因此，我們經過分析，得到結論： 不能把 位置的 橫縱座標 直接寫死了，而是應該 根據不同情況，動態計算這個座標值；
+      // 4. 經過分析，得出解題思路，計算小球從起始位置到圖示上徽標(badge)的位移的距離： 
+      //  4.1 先得到 購物車圖示上徽標(badge)的 橫縱 座標，
+      //  4.2 再得到 小球的 橫縱座標，
+      //  4.3 然後 讓 y 值 求差， x 值也求 差，得到 的結果，就是橫縱座標要位移的距離
+      // 5. 如何 獲取 徽標和小球的 位置？？？  javascript的 domObject.getBoundingClientRect() 方法。  (https://www.cnblogs.com/leejersey/p/4127714.html)
+      // 這個方法返回一個矩形對象，包含四個屬性：left、top、right和bottom。分別表示元素各邊與頁面 上邊 和 左邊 的距離。
+
+      // 獲取小球的 在頁面中的位置，ball是屬於當前元件，可以用 this.$refs 指到此dom
+      const ballPosition = this.$refs.ball.getBoundingClientRect(); // this.$refs.ref值 是Vue用來獲取元素的方法
+      // 獲取 徽標 在頁面中的位置
+      const badgePosition = document.getElementById("badge").getBoundingClientRect();
+
+      const xDist = badgePosition.left - ballPosition.left; // 橫向位移距離
+      const yDist = badgePosition.top - ballPosition.top; // 縱向位移距離
+
+      el.style.transform = `translate(${xDist}px, ${yDist}px)`; // 移到終點(使用模板字符串組出含變數的字串)
+
+
+
+
       // el.style.transition = "all 1s ease"; // 1秒的直線緩動動畫
       el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)"; // 0.5秒的貝茲曲線動畫
       // 貝茲曲線css產生器 https://cubic-bezier.com/#.4,-0.3,1,.68  #之後數值可以調整
+
       done();// 表示動畫完成，完成之後會呼叫 afterEnter
     },
     afterEnter(el) {// 動畫鉤子函數，動畫完成之後會呼叫這個 afterEnter
       this.ballFlag = !this.ballFlag; // 隱藏小球
+    },
+    getSelectedCount(count) {
+      // 當子元件把 選中的數量傳遞給父元件的時候，是把選中的值儲存到 data 上並用方法的參數傳給父元件
+      this.selectedCount = count;
+      console.log("父元件拿到的數量值為： " + this.selectedCount);
     }
   },
   components: {
--- src/components/subcomponents/goodsinfo_numbox.vue
+++ src/components/subcomponents/goodsinfo_numbox.vue
@@ -2,7 +2,8 @@
   <!-- 
   使用MUI的"number box（數字輸入框）"樣式：mui-master\examples\hello-mui\examples\numbox.html     
   -->
-  <div class="mui-numbox" data-numbox-min="1" data-numbox-max="9">
+  <!-- 測試MUI組件能不能成功 -->
+  <!-- <div class="mui-numbox" data-numbox-min="1" data-numbox-max="9">
     <button class="mui-btn mui-btn-numbox-minus" type="button">
       <font style="vertical-align: inherit;"><font style="vertical-align: inherit;">-</font></font>
     </button>
@@ -10,8 +11,20 @@
     <button class="mui-btn mui-btn-numbox-plus" type="button">
       <font style="vertical-align: inherit;"><font style="vertical-align: inherit;">+</font></font>
     </button>
-  </div>
+  </div> -->
 
+  <!-- data-numbox-max直接綁定參數寫法，但會獲得一個undefinde，所以這樣寫沒意義。(data-numbox-max是mui組件內部屬性) -->
+    <!-- <div class="mui-numbox" data-numbox-min="1" :data-numbox-max="max"> -->
+  <!-- 改使用JS寫法，在watch監聽max屬性 -->
+  <!-- 問題： 因為父元件異步獲取數據的緣故，我們不知道什麼時候能夠拿到 max 屬性值，所以可能會獲得一個undefinde，但是，總歸有一刻會拿到，會得到一個真正的 max 屬性值 -->
+  <!-- 因此我們可以 使用 watch 屬性監聽，來 監聽 父元件傳遞過來的 max 屬性值，不管 watch 會被觸發幾次，但是，最後一次，肯定是一個 合法的 max 數值 -->  
+    <div class="mui-numbox" data-numbox-min='1'>
+      <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+      <input id="test" class="mui-input-numbox" type="number" value="1" @change="countChanged" ref="numbox" />
+      <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+
+  <!-- 分析： 子元件什麼時候把 數據傳遞給父元件 -->
 </template>
 
 <script>
@@ -21,13 +34,28 @@ export default {
   mounted() {
     // 初始化數字選擇框元件：mui在mui.init()中會自動初始化基本控件,但是 動態添加的Numbox組件需要手動初始化，Vue內使用就是動態添加，所以需要手動初始化。
     mui(".mui-numbox").numbox();
+    // console.log('子元件取得父元件傳值，庫存量為 '+this.max);
   },
   methods: {
 
-  },
-  props: [],
-  watch: {
+    countChanged() {
+      // 每當 文字框的數據被修改的時候，立即把 最新的數據，通過事件呼叫，傳遞給父元件
+      console.log('子元件的值為： '+this.$refs.numbox.value);
+      this.$emit("getcount", parseInt(this.$refs.numbox.value));
+    }
 
+  },
+  props: ["max"],
+  watch: {    
+    // 詳細說明參考 day5\筆記\watch屬性
+    // 屬性監聽
+    max: function(newVal, oldVal) {
+      // 使用 MUI框架 中 numbox組件 的JS API 設定 numbox 的最大值，到達最大值（庫存值）後無法再按＋
+      mui(".mui-numbox")
+        .numbox()
+        .setOption("max", newVal);
+    }
+    
   }
 };
 </script>
